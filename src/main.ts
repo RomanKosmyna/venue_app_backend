500bf0ec2055ec3ee2093c7d76a793f7f94492cf
RomanKosmyna <roma.kosmyna@gmail.com>
2023-11-13 19:43:12 +0200

    Initialized project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const port = 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  console.log(`Starting server on ${port} port`);
+  await app.listen(port);
+}
+bootstrap();
