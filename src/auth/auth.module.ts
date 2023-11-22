fb24ea6054636f9e5a3e6229a8d0e6f6bc9374c7
RomanKosmyna <roma.kosmyna@gmail.com>
2023-11-22 16:19:45 +0200

    Added jwt package and generateJwt method

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     controllers: [AuthController],
     providers: [AuthService, PrismaService],
-    imports: [forwardRef(() => UsersModule)],
+    imports: [
+        forwardRef(() => UsersModule),
+        JwtModule.register({ secret: 'hard!to-guess_secret' })],
     exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private jwtService: JwtService
+    ) { }
+
     async hashPassword(password: string): Promise<string> {
         const saltOrRounds = 10;
         return await bcrypt.hash(password, saltOrRounds);
@@ -10,5 +15,13 @@ export class AuthService {
 
     async comparePassword(enteredPassword: string, hashedPassword: string): Promise<boolean> {
         return await bcrypt.compare(enteredPassword, hashedPassword);
+    }
+
+    async generateJwtToken(userId: string, email: string) {
+        const payload = {sub: userId, email: email};
+
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        };
     }
 }
